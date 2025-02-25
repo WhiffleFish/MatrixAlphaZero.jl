5c80893882c121634bd7fb769b8583557b5d2b5a
WhiffleFish <tylertronics@gmail.com>
2025-02-25 16:59:53 -0700

    monitor network training

--- src/train.jl
+++ src/train.jl
@@ -1,15 +1,20 @@
 function train!(sol, oracle, buf)
     (; batchsize, steps_per_iter, train_intensity, optimiser) = sol
     n_batches = (steps_per_iter * train_intensity) ÷ batchsize
-    
     opt_state = Flux.setup(optimiser, oracle)
+    losses = Float32[]
     for _ in 1:n_batches
         X, y = get_batch(buf, batchsize)
         ∇θ = Flux.gradient(oracle) do oracle
-            return Flux.mse(dropdims(oracle(X); dims=1), y)
+            l = Flux.mse(dropdims(oracle(X); dims=1), y)
+            Flux.Zygote.ignore_derivatives() do
+                push!(losses, l)
+            end
+            return l
         end
         Flux.update!(opt_state, oracle, ∇θ[1])
     end
+    return (;losses)
 end
 
 function get_batch(buf::Buffer, batchsize::Int)
