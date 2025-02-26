76a185cc052b8d199fa37e973fbdfbd41915cae3
WhiffleFish <tylertronics@gmail.com>
2025-02-25 17:00:55 -0700

    TensorGames handles COST NOT UTILITY fuck me

--- src/matrix.jl
+++ src/matrix.jl
@@ -1,6 +1,16 @@
+# NOTE: IMPORTANT - TensorGames.jl reasons about COST matrices NOT Utility matrices
+
 function solve(A::AbstractMatrix)
-    eq = compute_equilibrium([A, -A])
+    eq = compute_equilibrium([-A, A])
     x,y = normalize!(eq.x[1],1), normalize!(eq.x[2],1)
+    t = dot(x, A, y)
+    return x,y,t
+end
+
+function solve(A::AbstractMatrix, B::AbstractMatrix)
+    eq1 = compute_equilibrium([-A, A])
+    eq2 = compute_equilibrium([B, -B])
+    x,y = normalize!(eq1.x[1],1), normalize!(eq2.x[2],1)
     t = dot(x, A, y)
     return x,y,t
 end
