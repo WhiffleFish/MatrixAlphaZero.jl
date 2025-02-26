9aa8d8bc792333b2dd52fcfd8c2f73db5b43aeb5
WhiffleFish <tylertronics@gmail.com>
2025-02-26 15:42:21 -0700

    solve returns extra info

--- src/solver.jl
+++ src/solver.jl
@@ -26,18 +26,37 @@ end
     oracle::Oracle
 end
 
-function MarkovGames.solve(sol::AlphaZeroSolver, game::MG)
+function MarkovGames.solve(sol::AlphaZeroSolver, game::MG; s0=initialstate(game))
     mcts_iter = sol.steps_per_iter ÷ sol.mcts_params.max_depth
     total_iter = mcts_iter * sol.max_iter
     p = Progress(total_iter)
     buf = Buffer(sol.buff_cap)
+    train_losses = Vector{Float32}[]
     for i ∈ 1:sol.max_iter
         for _ ∈ 1:mcts_iter
-            hist = mcts_sim(sol.mcts_params, game, rand(initialstate(game)))
+            hist = mcts_sim(sol.mcts_params, game, rand(s0))
             push!(buf, hist)
             next!(p)
         end
-        train!(sol, sol.mcts_params.oracle, buf)
+        train_info = train!(sol, sol.mcts_params.oracle, buf)
+        push!(train_losses, train_info[:losses])
     end
-    return AlphaZeroPlanner(sol.mcts_params.oracle)
+    return AlphaZeroPlanner(sol.mcts_params.oracle), (;
+        train_losses, buffer=buf
+    )
+end
+
+function oracle_matrix_game(game, oracle, s)
+    γ = discount(game)
+    A1, A2 = actions(game)
+    mat = zeros(length(A1), length(A2))
+    for (i, a1) ∈ enumerate(A1)
+        for (j, a2) ∈ enumerate(A2)
+            a = (a1, a2)
+            sp, r = @gen(:sp, :r)(game, s, a)
+            vp = only(oracle(MarkovGames.convert_s(Vector{Float32}, sp, game)))
+            mat[i,j] = r + γ * vp
+        end
+    end
+    return mat
 end
