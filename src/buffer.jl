12ce101afff904f970dc32f6153822066b873589
WhiffleFish <tylertronics@gmail.com>
2025-03-02 21:20:37 -0700

    store mcts policy in buffer

--- src/buffer.jl
+++ src/buffer.jl
@@ -1,14 +1,21 @@
 @proto struct Buffer
-    const s::CircularBuffer{Vector{Float32}}
-    const v::CircularBuffer{Float32}
+    s::CircularBuffer{Vector{Float32}}
+    v::CircularBuffer{Float32}
+    p::NTuple{2,CircularBuffer{Vector{Float32}}}
 end
 
-Buffer(l::Int) = Buffer(CircularBuffer{Vector{Float32}}(l), CircularBuffer{Float32}(l))
+Buffer(l::Int) = Buffer(
+    CircularBuffer{Vector{Float32}}(l), 
+    CircularBuffer{Float32}(l),
+    (CircularBuffer{Vector{Float32}}(l), CircularBuffer{Vector{Float32}}(l))
+)
 
 function Base.push!(buf::Buffer, t::NamedTuple)
-    @assert length(t.v) == length(t.s)
+    @assert length(t.v) == length(t.s) == length(t.policy[1]) == length(t.policy[2])
     append!(buf.s, t.s)
     append!(buf.v, t.v)
+    append!(buf.p[1], t.policy[1])
+    append!(buf.p[2], t.policy[2])
     return buf
 end
 
