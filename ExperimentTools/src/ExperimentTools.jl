b2217bb2bf265ecb836010c4030348676677ab40
WhiffleFish <tylertronics@gmail.com>
2025-05-16 16:28:56 -0600

    exploitability stuff precompiles at least

--- ExperimentTools/src/ExperimentTools.jl
+++ ExperimentTools/src/ExperimentTools.jl
@@ -3,6 +3,13 @@ module ExperimentTools
 using MarkovGames
 using POSGModels
 using RecipesBase
+using POMDPTools
+using MCTS
+using Distributed
+using JLD2
+using MatrixAlphaZero
+using Random
 
+include("exploitability.jl")
 
 end # module ExperimentTools
