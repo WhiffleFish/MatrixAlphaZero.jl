07582f73882fe86678421a26cae1e5d49351e417
WhiffleFish <tylertronics@gmail.com>
2025-06-04 11:08:01 -0600

    working exploitability calc

--- ExperimentTools/src/ExperimentTools.jl
+++ ExperimentTools/src/ExperimentTools.jl
@@ -5,10 +5,13 @@ using POSGModels
 using RecipesBase
 using POMDPTools
 using MCTS
+using Flux
 using Distributed
 using JLD2
 using MatrixAlphaZero
 using Random
+using MatrixAlphaZero
+const AZ = MatrixAlphaZero
 
 include("exploitability.jl")
 
