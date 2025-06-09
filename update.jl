865c052096afb0b103b1863ffa4478bbd81e760c
WhiffleFish <tylertronics@gmail.com>
2025-06-09 17:08:29 -0600

    global update script

---
+++ update.jl
@@ -0,0 +1,13 @@
+using Pkg
+
+Pkg.activate(@__DIR__)
+@info "updating $(Pkg.project().name)..."
+Pkg.update()
+
+Pkg.activate(joinpath(@__DIR__, "ExperimentTools"))
+@info "updating $(Pkg.project().name)..."
+Pkg.update()
+
+Pkg.activate(joinpath(@__DIR__, "experiments"))
+@info "updating experiments..."
+Pkg.project()
