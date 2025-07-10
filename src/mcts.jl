a61d857d772f5c6f9863227d85dc1306f121ca25
WhiffleFish <tylertronics@gmail.com>
2025-07-10 14:25:56 -0400

    mcts search value estimate fix

--- src/mcts.jl
+++ src/mcts.jl
@@ -1,28 +1,30 @@
-@kwdef struct MCTSParams{T, Oracle}
+@kwdef struct MCTSParams{T, Oracle, MS}
     tree_queries    :: Int      = 10
     c               :: Float64  = 1.0
     max_depth       :: Int      = 100
     temperature     :: T        = t -> 1.0 * (0.95 ^ (t-1))
     max_time        :: Float64  = Inf
+    matrix_solver   :: MS       = RegretSolver(20)
     oracle          :: Oracle
 end
 
 uniform(n::Int) = fill(inv(n), n)
 
 function search_info(params::MCTSParams, game::MG, s; temperature=1.0)
+    (;matrix_solver) = params
     tree = Tree(game, s)
     x,y,v = if isterminal(game, s)
         n1, n2 = length.(actions(game))
         uniform(n1), uniform(n2), 0.0
     elseif iszero(params.max_depth) || iszero(params.tree_queries) || iszero(params.max_time)
-        solve(oracle_matrix_game(game, params.oracle, s))
+        solve(matrix_solver, oracle_matrix_game(game, params.oracle, s))
     else
         (;tree_queries, c) = params
         γ = discount(game)
         for i ∈ 1:tree_queries
             simulate(params, tree, game, 1; temperature)
         end
-        solve(node_matrix_game(tree, c, 1, γ))
+        solve(matrix_solver, node_matrix_game(tree, c, 1, γ))
     end
     return (x,y,v), (;tree)
 end
@@ -32,7 +34,7 @@ function search(params::MCTSParams, game::MG, s; temperature=1.0)
 end
 
 function simulate(params, tree, game, s_idx; temperature=1.0)
-    (; c, oracle) = params
+    (; c, oracle, matrix_solver) = params
     γ = discount(game)
     s = tree.s[s_idx]
     ns = tree.n_s[s_idx]
@@ -41,22 +43,25 @@ function simulate(params, tree, game, s_idx; temperature=1.0)
         return 0.0
     elseif is_leaf(tree, s_idx)
         expand_s!(tree, s_idx, game, oracle)
-        x,y,t = solve(node_matrix_game(tree, c, s_idx, γ))
+        x,y,t = solve(matrix_solver, node_matrix_game(tree, c, s_idx, γ))
         return t
     else
         # choose best action for exploration
-        a = explore_action(tree, c, s_idx, γ; temperature)
+        a = explore_action(matrix_solver, tree, c, s_idx, γ; temperature)
         sp_idx = tree.s_children[s_idx][a]
-        v_sample = simulate(params, tree, game, sp_idx; temperature)
+        v_sample = tree.r[s_idx][a] + γ * simulate(params, tree, game, sp_idx; temperature)
+
 
         # update node stats
         v̂ = tree.v[s_idx][a]
-        tree.v[s_idx][a]    += (v_sample - v̂) / (ns + 1)
+        nsa = tree.n_sa[s_idx][a]
+        # tree.v[s_idx][a]     = v_sample # (v_sample - v̂) / (ns + 1)
+        tree.v[s_idx][a]     = v̂ + (v_sample - v̂) / (nsa + 1)
         tree.n_s[s_idx]     += 1
         tree.n_sa[s_idx][a] += 1
 
         # solve game with updated statistics
-        # x,y,t = solve(node_matrix_game(tree, c, s_idx, γ))
+        # x,y,t = solve(matrix_solver, node_matrix_game(tree, c, s_idx, γ))
         return v_sample
     end
 end
@@ -85,8 +90,8 @@ function node_matrix_game(tree::Tree, c, s_idx, γ)
     return r .+ γ .* v
 end
 
-function explore_action(tree, c, s_idx, γ; temperature=1.0)
-    x,y,t = solve(ucb_matrix_games(tree, c, s_idx, γ; temperature)...)
+function explore_action(matrix_solver, tree::Tree, c::Float64, s_idx::Int, γ::Float64; temperature=1.0)
+    x,y,t = solve(matrix_solver, ucb_matrix_games(tree, c, s_idx, γ; temperature)...)
     return action_idx_from_probs(x,y)
 end
 
