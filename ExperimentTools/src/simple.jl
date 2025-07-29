f6049da196f9dd77e0094b6ec089df465a38bf5a
WhiffleFish <tylertronics@gmail.com>
2025-07-29 14:26:31 -0600

    add behavior to simple oracle

--- ExperimentTools/src/simple.jl
+++ ExperimentTools/src/simple.jl
@@ -4,7 +4,18 @@ struct SimpleOracle{D<:Dict} <: Policy
     d::D
 end
 
-SimpleOracle(game::SimpleMG) = SimpleOracle(SimpleGame.exact_solve(RegretSolver(20), game))
+function MarkovGames.behavior(oracle::SimpleOracle, s)
+    na1, na2 = length.(first(values(oracle.d)).policy)
+    res = get(oracle.d, s, nothing)
+    if isnothing(res)
+        return ProductDistribution(SparseCat(1:na1, AZ.uniform(na1)), SparseCat(1:na2, AZ.uniform(na2)))
+    else # stored entry
+        p1, p2 = res.policy[1], res.policy[2]
+        return ProductDistribution(SparseCat(1:na1, p1), SparseCat(1:na2, p2))
+    end
+end
+
+SimpleOracle(game::SimpleMG; solver=PATHSolver()) = SimpleOracle(SimpleGame.exact_solve(solver, game))
 
 function AZ.state_value(oracle::SimpleOracle, game, s)
     haskey(oracle.d, s) ? oracle.d[s].value : 0.0
