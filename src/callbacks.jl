3ca907e0f8fee9603bd1e922ce61df775abe8333
WhiffleFish <tylertronics@gmail.com>
2026-06-15 13:29:27 -0600

    prep new dubin experiment

--- src/callbacks.jl
+++ src/callbacks.jl
@@ -23,8 +23,10 @@ function (cb::MetricsCallback)(info::NamedTuple)
     bar = "─" ^ cb.width
     println(bar)
     println("  iter $(info.iter)")
-    if hasproperty(info, :mean_loss)
+    if hasproperty(info, :mean_regret_loss)
         println("  loss:        total=$(_r(info.mean_loss))  value=$(_r(info.mean_value_loss))  regret=$(_r(info.mean_regret_loss))  strategy=$(_r(info.mean_strategy_loss))")
+    elseif hasproperty(info, :mean_policy_loss)
+        println("  loss:        total=$(_r(info.mean_loss))  value=$(_r(info.mean_value_loss))  policy=$(_r(info.mean_policy_loss))")
     end
     if hasproperty(info, :mean_grad_norm)
         println("  grad norm:   mean=$(_r3(info.mean_grad_norm))  max=$(_r3(info.max_grad_norm))")
@@ -36,6 +38,15 @@ function (cb::MetricsCallback)(info::NamedTuple)
     if hasproperty(info, :target_strategy_kl_p1)
         target_regret_l2 = hasproperty(info, :target_regret_l2) ? "  target regret l2=$(_r(info.target_regret_l2))" : ""
         println("  oracle↔target kl:  p1=$(_r(info.target_strategy_kl_p1))  p2=$(_r(info.target_strategy_kl_p2))  │  regret mse=$(_r(info.regret_pred_mse))$(target_regret_l2)  value mse=$(_r(info.value_pred_mse))")
+    end
+    if hasproperty(info, :policy_entropy_p1)
+        println("  entropy:     p1=$(_r3(info.policy_entropy_p1))  p2=$(_r3(info.policy_entropy_p2))")
+        println("  policy Δkl:  p1=$(_r(info.policy_kl_p1))  p2=$(_r(info.policy_kl_p2))")
+    end
+    if hasproperty(info, :target_policy_kl_p1)
+        target_regret_l2 = hasproperty(info, :target_regret_l2) ? "  target regret l2=$(_r(info.target_regret_l2))" : ""
+        regret_mse = hasproperty(info, :regret_pred_mse) ? "  │  regret mse=$(_r(info.regret_pred_mse))$(target_regret_l2)" : ""
+        println("  oracle↔target kl:  p1=$(_r(info.target_policy_kl_p1))  p2=$(_r(info.target_policy_kl_p2))$(regret_mse)  value mse=$(_r(info.value_pred_mse))")
     end
     if hasproperty(info, :mean_ep_length)
         println("  self-play:   ep_len=$(_r(info.mean_ep_length, 1))  reward μ=$(_r3(info.mean_reward))  σ=$(_r3(info.reward_std))")
